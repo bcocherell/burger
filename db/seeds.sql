c25049a55a3fbb85e5a8e703f6178b5f04d90c25
Brian Cocherell <brian.cocherell@gmail.com>
2018-02-06 19:37:30 -0500

    functioning site

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) values ('Mission A-Corn-Plished Burger');
+insert into burgers (burger_name) values ('Foot Feta-ish Burger');
+insert into burgers (burger_name) values ('New Bacon-ings Burger');
